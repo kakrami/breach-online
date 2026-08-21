33fe03e05d7451e8a87a273beca6fb7646093a4d
kakrami <78782842+kakrami@users.noreply.github.com>
2026-08-20 19:34:22 -0700

    Add files via upload

--- game-config.js
+++ game-config.js
@@ -1,5 +1,5 @@
-export const APP_VERSION = '1.17.0';
-export const PROTOCOL_VERSION = 36;
+export const APP_VERSION = '1.17.1';
+export const PROTOCOL_VERSION = 37;
 export const ROOM_CODE_LENGTH = 4;
 export const MAX_PLAYERS = 8;
 export const MAX_BOTS = 8;
